04a94681a6e441decfe65fc95455539626b2d0a4
TeBeCo <tebeco@gmail.com>
2020-09-06 16:36:59 +0200

    wip dotnet-install

--- build.cmd
+++ build.cmd
@@ -7,6 +7,14 @@ if errorlevel 1 (
 
 setlocal
 
+set DOTNET_INSTALL_FOLDER=%~dp0.dotnet
+set DOTNET_VERSION=3.1.401
+
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; &([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://dot.net/v1/dotnet-install.ps1'))) -InstallDir "$($env:DOTNET_INSTALL_FOLDER)" -Version "$($env:DOTNET_VERSION)""
+set DOTNET_ROOT=%DOTNET_INSTALL_FOLDER%
+set DOTNET_MULTILEVEL_LOOKUP=0
+set PATH=%DOTNET_ROOT%;%PATH%
+
 set MSBuild=%~dp0packages\build\RoslynTools.MSBuild\tools\msbuild
 
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
